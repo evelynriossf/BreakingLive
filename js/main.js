fd066a1b1cd0032b51f83e6334860e577e433858
Evelyn Rios <everios@me.com>
2014-06-01 09:02:41 -0700

    CSS changes

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// angular.module('myApp').controller('GalleryCtrl', ['$scope', function($scope) {
-//   $scope.assets = Asset.query({album: 'shortcut'});
-// }]);
-
-  angular.module('Test', ['chute']).controller('MainCtrl', ['$scope', 'Chute.API.Asset', function($scope, Asset) {
-        $scope.assets = Asset.query({album: 'abcqsrlx', perPage: 3});
-      }]);
-    
